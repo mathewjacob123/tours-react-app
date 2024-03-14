8a09a24e5ffe2c8138bdafb6da024786e8936896
Mathew Jacob <mathewjacob2503@gmail.com>
2024-03-14 12:24:43 +0530

    changed header

--- ReactWorkshop/src/Components/Header.jsx
+++ ReactWorkshop/src/Components/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
   return (
     <div className="container">
-      <h1 className="title">Plan a Tour</h1>
+      <h1 className="title">Happy Journey</h1>
       </div>
   )
 }
